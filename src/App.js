8a8196d9b0a470a0a42e9d9c38cdb8d1b2f3ef3d
naresh <nareshcena666@gmail.com>
2024-03-21 21:02:36 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+// import data from '../key.json';
+import axios from 'axios';
+import React, { useRef, useState } from 'react'
 
 function App() {
+  const inputRef = useRef();
+  const [imageLink, setImageLink] = useState();
+
+  const url = "https://api-inference.huggingface.co/models/prompthero/openjourney-v4";
+
+  function clickHandler(){
+
+      const data = {
+          "input": `${inputRef.current.value}`
+      }
+
+
+      axios.post(url,{
+          inputs:JSON.stringify(data)
+      },{
+          headers:{
+              Autherization: data.access_token
+          },
+          responseType: "blob"
+      })
+      .then((res)=>{
+          const image = URL.createObjectURL(res.data);
+          setImageLink(image)
+      })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+            <h1>Image generator</h1>
+        </header>
+        <div id='input-container'>
+            <input ref={inputRef} type='text' placeholder='Search for the image...'/>
+            <button onClick={clickHandler}>Generate</button>
+        </div>
+        <div id='image-container'>
+            {imageLink && <img src = {imageLink} alt={`image of ${inputRef.current?.value}`}/>}
+        </div>
     </div>
   );
 }
